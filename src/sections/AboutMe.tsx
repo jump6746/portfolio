78e7f29fb98730f36c2912eb2dc22d98d6f33a21
jump6746 <jump6746@naver.com>
2025-09-11 21:28:28 +0900

    디자인 수정

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -12,7 +12,7 @@ const AboutMe = () => {
           <h4 className="text-blue-600 font-semibold text-lg sm:text-xl md:text-2xl text-center">
             프론트엔드 역량
           </h4>
-          <p className="font-medium text-gray-600 text-sm sm:text-base whitespace-nowrap text-center">
+          <p className="font-medium text-gray-800 text-sm sm:text-base whitespace-nowrap text-center p-5 bg-sky-50 rounded-xl">
             React와 Vue 기반의 프론트엔드 개발 경험이 있으며, <br />
             Javascript와 Typescript에 능숙합니다. <br /> Next.js와 다양한 React
             라이브러리를 활용한 <br /> 프로젝트 경험이 있습니다.
@@ -22,7 +22,7 @@ const AboutMe = () => {
           <h4 className="text-blue-600 font-semibold text-lg sm:text-xl md:text-2xl text-center">
             협업 및 커뮤니케이션
           </h4>
-          <p className="font-medium text-gray-600 text-sm sm:text-base whitespace-nowrap text-center">
+          <p className="font-medium text-gray-800 text-sm sm:text-base whitespace-nowrap text-center p-5 bg-sky-50 rounded-xl">
             GitHub, GitLab을 활용한 협업 경험이 있으며, <br /> Jira, Notion 등
             협업 도구에도 익숙합니다. <br /> 기획, 디자인, 백엔드 직무 경험을
             바탕으로 <br /> 원활한 크로스 직군 협업이 가능합니다.
@@ -32,7 +32,7 @@ const AboutMe = () => {
           <h4 className="text-blue-600 font-semibold text-lg sm:text-xl md:text-2xl text-center">
             효율적인 웹 아키텍처 설계
           </h4>
-          <p className="font-medium text-gray-600 text-sm sm:text-base whitespace-nowrap text-center">
+          <p className="font-medium text-gray-800 text-sm sm:text-base whitespace-nowrap text-center p-5 bg-sky-50 rounded-xl">
             API 호출 전략을 수립하고 Web Worker를 적용해 <br /> 서버 리소스를
             절감했습니다. <br /> 프로젝트 전반에서 활용 가능한 유틸 함수들을
             정리하여 <br /> 유지보수성과 가독성을 높였습니다.
